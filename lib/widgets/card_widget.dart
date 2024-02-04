d3dfeb3d24c5fd4d3ed913bfb43e1f5806ff7c1b
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-04 10:15:25 -0500

    appbar

---
+++ lib/widgets/card_widget.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class CardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(14),
+      width: 160,
+      height: 220,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(22.0),
+        color: Colors.red,
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.40),
+            BlendMode.srcATop,
+          ),
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              "https://st2.depositphotos.com/4035913/6964/i/950/depositphotos_69640011-stock-photo-venice-italy.jpg"),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.35),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  "NEW",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    left: 40,
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Thailand",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "18 tours",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 4),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.35),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    Text(
+                      "4.5",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
