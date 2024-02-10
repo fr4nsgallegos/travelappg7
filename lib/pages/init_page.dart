a8543254eecfa5adac690631b26be79797edf787
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-02-09 20:15:13 -0500

    optimizando codigo

--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelappg7/widgets/welcome_widget.dart';
 
 class InitPage extends StatelessWidget {
   @override
@@ -8,42 +9,11 @@ class InitPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height - 100,
-              width: MediaQuery.of(context).size.width - 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
+            WelcomeWidget(
+                title: "Viaja",
+                description: "esta es la descrp",
                 color: Colors.orange,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/icons/avion.png",
-                    height: MediaQuery.of(context).size.height / 4,
-                  ),
-                  SizedBox(height: 40),
-                  Text(
-                    "Viaja",
-                    style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue consectetur ante eu lobortis. Phasellus tempor ligula neque, ut tincidunt dolor ultricies vitae. Nam aliquet scelerisque nisi. Vestibulum placerat orci risus, id pretium mi malesuada sed",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
+                pathAsset: "assets/icons/avion.png"),
           ],
         ),
       ),
---
+++ lib/widgets/welcome_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class WelcomeWidget extends StatelessWidget {
+  String title;
+  String description;
+  Color color;
+  String pathAsset;
+
+  WelcomeWidget({
+    required this.title,
+    required this.description,
+    required this.color,
+    required this.pathAsset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height - 100,
+      width: MediaQuery.of(context).size.width - 40,
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(25), color: color),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            pathAsset,
+            height: MediaQuery.of(context).size.height / 4,
+          ),
+          SizedBox(height: 40),
+          Text(
+            title,
+            style: TextStyle(
+                fontSize: 35, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              description,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
